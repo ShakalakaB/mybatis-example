e0c23f8ee391315cc47d14030346dae7730f62b5
ShakalakaB <yunjie989@gmail.com>
2021-06-10 17:20:20 +0800

    Init project

---
+++ src/test/java/aldora/mybatis/example/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package aldora.mybatis.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
